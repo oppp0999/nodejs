845c60b4d81610afd7ee4d00b9b1ae9f05f86654
oppp0999 <oppp0999@naver.com>
2022-08-07 18:35:24 +0900

    20220807_1

--- nodejs_form_UI/formui_create6.js
+++ nodejs_form_UI/formui_create6.js
@@ -54,7 +54,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+              `<a href="/create">create</a>
+               <a href="/update?id=${title}">update</a>
+               <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>`);
             response.writeHead(200);
             response.end(template);
           });
@@ -137,6 +142,20 @@ var app = http.createServer(function(request,response){
                   });
               });
             }
+      else if(pathname === '/delete_process'){
+        var body = '';
+        request.on('data', function(data){
+        body = body + data;
+      });
+        request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`../nodejs_read/data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
+      });
+    }
     else {
       response.writeHead(404);
       response.end('Not found');
