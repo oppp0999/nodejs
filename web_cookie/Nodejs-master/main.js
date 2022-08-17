5ac6e8d19acb0aa031ebebe42709a5eb33e36e72
oppp0999 <oppp0999@naver.com>
2022-08-17 16:26:09 +0900

    20220817_1

--- web_cookie/Nodejs-master/main.js
+++ web_cookie/Nodejs-master/main.js
@@ -5,11 +5,49 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var cookie = require('cookie');
+const { request } = require('https');
+
+function authIsOwner(request, response){
+  //쿠키 정보를 위해서 리퀘스트와 리스폰스를 모두 받음
+  var isOwner = false;
+  var cookies = {}
+  if(request.headers.cookie){
+    cookies = cookie.parse(request.headers.cookie);} //if
+  if(cookies.email === 'oppp0999@naver.com' && cookies.password === '1234'){
+    isOwner = true;
+
+}
+  return isOwner;
+}
+
+function authStatusUI(request, response){
+  var authStatusUI = '<a href="/login">login</a>';
+    if(authIsOwner(request,response)){
+      authStatusUI = '<a href="/logout_process">logout</a>';
+    } 
+    return authStatusUI;
+  }
+
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
+  
+    /* var cookies = {}
+    if(request.headers.cookie){
+      cookies = cookie.parse(request.headers.cookie);} //if
+    if(cookies.email === 'oppp0999@naver.com' && cookie.password === '1234'){
+      isOwner = true;
+    } 함수 처리하기
+ */
+    
+   /*  var authStatusUI = '<a href="/login">login</a>';
+    if(isOwner){
+      authStatusUI = '<a href="/login">logout</a>';
+    } */
+
     if(pathname === '/'){
       if(queryData.id === undefined){
         fs.readdir('./data', function(error, filelist){
@@ -18,7 +56,8 @@ var app = http.createServer(function(request,response){
           var list = template.list(filelist);
           var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
+            `<a href="/create">create</a>`,
+            authStatusUI(request, response) //맨 밑에 두기 왜냐하면 템플릿에 있는 파라미터
           );
           response.writeHead(200);
           response.end(html);
@@ -40,7 +79,7 @@ var app = http.createServer(function(request,response){
                 <form action="delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
-                </form>`
+                </form>`, authStatusUI(request,response)
             );
             response.writeHead(200);
             response.end(html);
@@ -61,11 +100,15 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
+        `, '', authStatusUI(request,response));
         response.writeHead(200);
         response.end(html);
       });
     } else if(pathname === '/create_process'){
+      if(authIsOwner(request,response) === false){
+        response.end('Login required!!')
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -98,7 +141,7 @@ var app = http.createServer(function(request,response){
               </p>
             </form>
             `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`, authStatusUI(request,response)
           );
           response.writeHead(200);
           response.end(html);
@@ -135,7 +178,71 @@ var app = http.createServer(function(request,response){
             response.end();
           })
       });
-    } else {
+    } 
+    else if(pathname === '/login'){
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Login';
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
+            `<form action="login_process" method="post">
+              <p><input type="text" name="email" placeholder="email"></p>
+              <p><input type="password" name="password" placeholder="password"></p> 
+              <p><input type="submit"</p>
+
+            </form>
+            `,
+            `<a href="/create">create</a>`
+          );
+          //타입이 패스워드일때
+          response.writeHead(200);
+          response.end(html);
+        });
+      } 
+      else if(pathname === '/login_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            if(post.email === 'oppp0999@naver.com' && post.password === '1234'){
+              response.writeHead(302, {
+                'Set-Cookie':[
+                  `email=${post.email}`,
+                  `password= ${post.password}`,
+                  `nickname=oppp`
+                ],
+                Location: `/`} //response.writehead_2
+                ); //response.writehead
+                response.end();
+              }//if
+            
+            else {
+            response.end('who');
+            }//else
+        });
+      } 
+
+      else if(pathname === '/logout_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            
+              response.writeHead(302, {
+                'Set-Cookie':[
+                  `email=; Max-Age=0`,
+                  `password=; Max-Age=0`,
+                  `nickname=; Max-Age=0`
+                ],
+                Location: `/`} //response.writehead_2
+                ); //response.writehead
+                response.end();
+        });
+      } 
+    else {
       response.writeHead(404);
       response.end('Not found');
     }
--- web_cookie/Nodejs-master/nodejs/cookie.js
+++ web_cookie/Nodejs-master/nodejs/cookie.js
@@ -3,10 +3,23 @@ var cookie = require('cookie');
 
 http.createServer(function(request, response){
     console.log(request.headers.cookie);
-    var cookies = cookie.parse(request.headers.cookie);
+
+    var cookies = {};
+    if(request.headers.cookie !== undefined){
+        cookies = cookie.parse(request.headers.cookie);
+    }
+
+    
     console.log(cookies.yummy_cookie);
     response.writeHead(200, {
-        'Set-Cookie' : ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie' : ['yummy_cookie=choco', 
+        'tasty_cookie=strawberry',
+        `Permanent=cookies; Max-Age=${60*60*24*30}`,
+        'Secure=Secure; Secure',
+        'HttpOnly=HttpOnly; HttpOnly',
+        'Path=Path; Path=/cookie', 
+        'Doamin=Domain; Domain=test.o2.org'
+    ]
     });
 
     response.end('Cookie!!');
